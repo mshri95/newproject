ea8eaac833f3e83cf0df5389d9d061d2cdf75abd
Madhushri_Hm <mshri95@gmail.com>
2017-08-22 18:55:44 +0530

    third file

---
+++ A.java
@@ -0,0 +1,30 @@
+public class A
+
+{
+public static void main(String[] args) 
+{ 
+
+
+    System.out.println("   1"); 
+    for(int i=1;i<7;i++)
+    {
+     if(i==1)
+     {
+             System.out.println("1     2 ");
+     }
+     else if(i==3)
+     {
+         System.out.println("1333322");
+     }
+     else
+        {
+                System.out.print("1");
+                System.out.print("     ");
+                System.out.print("2");
+                    System.out.print("\n");
+     }
+    
+}
+    
+}
+}
